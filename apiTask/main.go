5076e4f70bf16850d40a9fbffbb7502026a41cfc
Darshil Mahraur <darshilmahraur67@gmail.com>
2025-02-10 00:21:11 +0530

    initialized all the microservices to be used

---
+++ apiTask/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("API Server is running"))
+	})
+
+	fmt.Printf("Server started at http://localhost:8083\n")
+	http.ListenAndServe(":8083", r)
+}
---
+++ dbTask/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("DB Server is running"))
+	})
+	fmt.Printf("Server started at http://localhost:8081\n")
+
+	http.ListenAndServe(":8081", r)
+}
